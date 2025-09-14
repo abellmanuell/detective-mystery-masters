0d23442ec05369689f94c780f0de77a4190986ba
abellmanuell <mannydev02@gmail.com>
2025-09-14 13:27:09 +0100

    Increase max-w on larger screen

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -8,7 +8,14 @@ function Container({
   className?: string;
 }) {
   return (
-    <div className={cn("mx-auto max-w-7xl p-4", className)}>{children}</div>
+    <div
+      className={cn(
+        "mx-auto max-w-7xl p-4 lg:max-w-[1400px] xl:max-w-[1500px]",
+        className,
+      )}
+    >
+      {children}
+    </div>
   );
 }
 
