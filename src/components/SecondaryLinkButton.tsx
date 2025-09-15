485bfe850ba06eb1e41d04a4d49091bc32cda3a1
abellmanuell <mannydev02@gmail.com>
2025-09-15 16:46:14 +0100

    fix: Reduce the secondary outline width

--- src/components/SecondaryLinkButton.tsx
+++ src/components/SecondaryLinkButton.tsx
@@ -21,7 +21,7 @@ const SecondaryLinkButton = ({
         {...props}
         href={href}
         className={cn(
-          "bg-transparent outline-2",
+          "bg-transparent outline-1",
           "inline-flex items-center space-x-4",
           "rounded-full px-7 py-4 font-bold uppercase",
           "duration-150 ease-in",
