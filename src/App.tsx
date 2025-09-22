1b90d185202991b53c493889f192a1f38e0e6b34
abellmanuell <mannydev02@gmail.com>
2025-09-22 18:17:02 +0100

    chore: Change view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-// import HomePageRoute from "./pages/routes/homepage/HomePageRoute";
-import CategoryRoute from "./pages/routes/category-page/CategoryRoute";
-import { shopAllCategory } from "./lib/globalVariables";
+import HomePageRoute from "./pages/routes/homepage/HomePageRoute";
+// import CategoryRoute from "./pages/routes/category-page/CategoryRoute";
+// import { shopAllCategory } from "./lib/globalVariables";
 
 const App = () => {
   return (
     <>
-      {/* <HomePageRoute /> */}
-      <CategoryRoute {...shopAllCategory} />
+      <HomePageRoute />
+      {/* <CategoryRoute {...shopAllCategory} /> */}
     </>
   );
 };
